8ff1ebbc210a9668dbe83d95e81a7e6c8316055b
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2026-02-10 14:08:42 +0000

    Added library barrel exports. Configured top-level buildDb factory function in barrel export file

--- lib/flutter_quick_db.dart
+++ lib/flutter_quick_db.dart
@@ -1,5 +1,14 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:build/build.dart';
+import 'package:flutter_quick_db/codegen/generator.dart';
+import 'package:source_gen/source_gen.dart';
+
+export 'package:flutter_quick_db/codegen/annotations.dart';
+export 'package:flutter_quick_db/data_store.dart';
+
+Builder buildDb(BuilderOptions options) {
+  return PartBuilder(
+    [QuickDatabaseGenerator()],
+    ".g.dart",
+    header: "// Generated code - do not modify by hand\n\n",
+  );
 }
